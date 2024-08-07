f271122ed560b03d5a6cafc1bc15b89fb171be73
jivan <jeevanaryal7947@gmail.com>
2024-08-07 14:16:09 +0545

    added the API_BASE_URL in api.ts file which helps us to manage the request from one file and it reduces time to when developer need to change api routes frequently

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import Layout from "./hoc/layout/Layout";
 import HomePage from "./pages/home-page";
 import NewArrivals from "./pages/New Arrivals/NewArrivals";
 import Sales from "./pages/sales-page/Sales";
-import About from "./pages/About-page/About";
+// import About from "./pages/About-page/About";
 import Shop from "./pages/shop-page/Shop";
 import HeroProduct from "./single-product-page/HeroProduct";
 
@@ -17,7 +17,7 @@ const App = () => {
           <Route path="/sales" element={<Sales />} />
           <Route path="/shop" element={<Shop />} />
           <Route path="/products/:id" element={<HeroProduct />} />
-          <Route path="/about-us" element={<About />} />
+          {/* <Route path="/about-us" element={<About />} /> */}
         </Route>
       </Routes>
     </Router>
